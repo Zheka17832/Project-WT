4385015cedae8f81c4e3a75063be4546696b359b
Khomiak.Yevhen <90573968+Zheka17832@users.noreply.github.com>
2025-01-26 20:29:22 +0200

    Update Painter.js

--- LW3/function-tasks/Painter.js
+++ LW3/function-tasks/Painter.js
@@ -1,4 +1,4 @@
-function Painter(color) {
+export default function Painter(color) {
     return function(object) {
         const type = object.type ? `Type: ${object.type}` : "No 'type' property occurred!";
         console.log(`${color} - ${type}`);
